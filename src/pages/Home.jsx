696a63d431936e2a849903931f8970435eb38eea
devszera <devsurve1620@gmail.com>
2025-10-11 15:50:38 +0530

    idk what changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -58,7 +58,7 @@ const Home = () => {
   ];
 
   return (
-    <div className="min-h-screen bg-burrow-background page-fade">
+    <div className="min-h-screen bg-burrow-primary page-fade">
         <section className="section-hero">
             <div className="layout-container">
               <div className="flex flex-col-reverse items-center gap-10 md:flex-row md:items-center">
